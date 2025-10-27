ad742f93ba81ed28353c46c4c559d799934e4f9e
Magnus Eriksson <eriksson.mag@gmail.com>
2025-10-27 14:44:18 +0100

    Fix service

--- service/bootstrap.sh
+++ service/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo install -D target/debug/linux-sensor-agent /usr/local/sbin
-sudo install -D service/sensor-agent.service /lib/systemd/system
-sudo install -D config.toml /etc/linux-sensor-agent/config.toml
+sudo install -D -m 0644 service/sensor-agent.service /lib/systemd/system
+sudo install -D -m 0644 config.toml /etc/linux-sensor-agent/config.toml
 sudo systemctl daemon-reload
 sudo systemctl start sensor-agent
 sudo systemctl enable sensor-agent
